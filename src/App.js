44537a5372812d6754be3e11339d5b9c8f4b1d5e
Sirivella Sandeep <sirivella.ramu26@gmail.com>
2023-05-01 23:39:51 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+/**
+ * Header
+ *  Logo
+ *  Nav items(like footer,About us, cart etc...)
+ *  Carousel(Mani events contact number)
+ * Body 
+ *  Cards(List of events which  they organize like decarations,Food etc....)
+ *      Cards(Sub types  types of decarations like haldi, Mehandii)
+ *              Image
+ *              Name
+ *          cards(list all decarations in that particular types )(Both images as well as Videos)
+ *                  Image
+ *                  Name 
+ *                  Price 
+ *                  Add to cart button 
+ * 
+ * 
+ * Footer: 
+ *  ALl the required information Like contact Number 
+ *  Logo 
+ *  Social Media links 
+ *  etc..
+ *      
+ * 
+ * 
+ */
